01aa592b2f3f547d516df5ffa5dd4809b8d75f67
Julian Strobl <stroblju@gmail.com>
2015-04-10 16:09:07 +0200

    show version instead of hello world for now

--- app/src/main/java/org/berlin_vegan/bvapp/MainListActivity.java
+++ app/src/main/java/org/berlin_vegan/bvapp/MainListActivity.java
@@ -1,6 +1,7 @@
 package org.berlin_vegan.bvapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class MainListActivity extends BaseActivity {
@@ -9,5 +10,8 @@ public class MainListActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_list_activity);
+        final String version = "Version: " + BuildConfig.VERSION_GIT_DESCRIPTION;
+        final TextView textView = (TextView) findViewById(R.id.version);
+        textView.setText(version);
     }
 }
