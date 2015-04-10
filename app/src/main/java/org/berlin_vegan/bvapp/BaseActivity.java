dc5c7184706f44b0b297d41be42f60c740b77fbf
Georgi <katzenbaby9@gmail.com>
2015-04-10 14:51:19 +0200

    - changed actionbar to toolbar - added color samples - green and light green - added a baseactivity class as a wrapper - added menu item

---
+++ app/src/main/java/org/berlin_vegan/bvapp/BaseActivity.java
@@ -0,0 +1,62 @@
+package org.berlin_vegan.bvapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by georgi on 4/10/15.
+ */
+public class BaseActivity extends ActionBarActivity {
+    private final Activity main = (Activity) this;
+    private Context c;
+    private Toolbar mToolbar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        this.c = getApplicationContext();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        //change the menu based on the calling activity
+        if (main instanceof MainListActivity) {
+            getMenuInflater().inflate(R.menu.menu_main_list, menu);
+        }
+        //else other activities
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                break;
+            case R.id.action_filter:
+                break;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/org/berlin_vegan/bvapp/MainListActivity.java
@@ -0,0 +1,13 @@
+package org.berlin_vegan.bvapp;
+
+import android.os.Bundle;
+
+
+public class MainListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_list_activity);
+    }
+}
