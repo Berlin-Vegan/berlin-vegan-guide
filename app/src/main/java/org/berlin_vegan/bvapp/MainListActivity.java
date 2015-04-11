85664bcd5d69df302ad78f1fb12695f232b7f003
Julian Strobl <stroblju@gmail.com>
2015-04-11 08:27:15 +0200

    remove dummy version text field

--- app/src/main/java/org/berlin_vegan/bvapp/MainListActivity.java
+++ app/src/main/java/org/berlin_vegan/bvapp/MainListActivity.java
@@ -1,7 +1,6 @@
 package org.berlin_vegan.bvapp;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 
 public class MainListActivity extends BaseActivity {
@@ -10,8 +9,5 @@ public class MainListActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_list_activity);
-        final String version = "Version: " + BuildConfig.VERSION_GIT_DESCRIPTION;
-        final TextView textView = (TextView) findViewById(R.id.version);
-        textView.setText(version);
     }
 }
