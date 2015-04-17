22678b8fb67105dd8de326b9b3392600b943e8de
Julian Strobl <stroblju@gmail.com>
2015-04-17 16:16:26 +0200

    add empty settings activity

---
+++ app/src/main/java/org/berlin_vegan/bvapp/SettingsActivity.java
@@ -0,0 +1,24 @@
+package org.berlin_vegan.bvapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame, new SettingsFragment()).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+        }
+    }
+}
