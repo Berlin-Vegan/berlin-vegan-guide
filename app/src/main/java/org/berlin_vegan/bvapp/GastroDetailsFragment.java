012d1787882f599fd48f3a27307cfe65014eb551
Julian Strobl <stroblju@gmail.com>
2015-04-25 09:23:51 +0200

    add opening hours to gastro details
    For the sake of simplicity every weekday is shown and not grouped by
    opening hours like in the old app.

--- app/src/main/java/org/berlin_vegan/bvapp/GastroDetailsFragment.java
+++ app/src/main/java/org/berlin_vegan/bvapp/GastroDetailsFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.Html;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class GastroDetailsFragment extends Fragment {
@@ -25,6 +28,7 @@ public class GastroDetailsFragment extends Fragment {
         } else {
             mGastroLocation = (GastroLocation) savedInstanceState.getSerializable("GASTRO_LOCATION");
         }
+        // contact
         if (mGastroLocation.getStreet() != null) {
             StringBuilder address = new StringBuilder()
                     .append("<a href=\"http://maps.google.com/maps?q=")
@@ -59,7 +63,45 @@ public class GastroDetailsFragment extends Fragment {
             vGastroDetailsTelephone.setText(Html.fromHtml(telephone.toString()));
             vGastroDetailsTelephone.setMovementMethod(LinkMovementMethod.getInstance());
         }
+        // opening hours
+        LinearLayout gastroDetailsOpeningHoursContent = (LinearLayout) v.findViewById(R.id.gastro_details_opening_hours_content);
+        String[][] data = {
+                {getString(R.string.gastro_details_opening_hours_content_monday), mGastroLocation.getOtMon()},
+                {getString(R.string.gastro_details_opening_hours_content_tuesday), mGastroLocation.getOtTue()},
+                {getString(R.string.gastro_details_opening_hours_content_wednesday), mGastroLocation.getOtWed()},
+                {getString(R.string.gastro_details_opening_hours_content_thursday), mGastroLocation.getOtThu()},
+                {getString(R.string.gastro_details_opening_hours_content_friday), mGastroLocation.getOtFri()},
+                {getString(R.string.gastro_details_opening_hours_content_saturday), mGastroLocation.getOtSat()},
+                {getString(R.string.gastro_details_opening_hours_content_sunday), mGastroLocation.getOtSun()}
+        };
+        for (int i = 0; i < data.length; i++) {
+            // view day of the week
+            TextView vDayOfTheWeek = new TextView(getActivity());
+            vDayOfTheWeek.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, getPx(36)));
+            vDayOfTheWeek.setEllipsize(TextUtils.TruncateAt.END);
+            vDayOfTheWeek.setGravity(Gravity.BOTTOM);
+            vDayOfTheWeek.setMaxLines(1);
+            vDayOfTheWeek.setText(data[i][0]);
+            vDayOfTheWeek.setTextColor(getResources().getColor(R.color.theme_primary));
+            gastroDetailsOpeningHoursContent.addView(vDayOfTheWeek);
+            // view opening hours
+            TextView vOpeningHours = new TextView(getActivity());
+            vOpeningHours.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            vOpeningHours.setMaxLines(1);
+            if (!data[i][1].trim().equals("")) {
+                vOpeningHours.setText(data[i][1]);
+            } else {
+                vOpeningHours.setText(getString(R.string.gastro_details_opening_hours_content_closed));
+            }
+            vOpeningHours.setTextColor(getResources().getColor(R.color.theme_primary_secondary_text));
+            gastroDetailsOpeningHoursContent.addView(vOpeningHours);
+        }
         return v;
+    }
+
+    private int getPx(int dimensionDp) {
+        float density = getResources().getDisplayMetrics().density;
+        return (int) (dimensionDp * density + 0.5f);
     }
 
     @Override
