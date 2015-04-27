59be725f222bffa9c767645517c3909c1f278959
Julian Strobl <stroblju@gmail.com>
2015-04-27 19:45:46 +0200

    improve gastro details fragment
    * rename variable for consistency reasons
    * always return 'unknown' if string is not found in json file

--- app/src/main/java/org/berlin_vegan/bvapp/GastroDetailsFragment.java
+++ app/src/main/java/org/berlin_vegan/bvapp/GastroDetailsFragment.java
@@ -65,7 +65,7 @@ public class GastroDetailsFragment extends Fragment {
         }
         // opening hours
         LinearLayout gastroDetailsOpeningHoursContent = (LinearLayout) v.findViewById(R.id.gastro_details_opening_hours_content);
-        String[][] data = {
+        String[][] openingHoursData = {
                 {getString(R.string.gastro_details_opening_hours_content_monday), mGastroLocation.getOtMon()},
                 {getString(R.string.gastro_details_opening_hours_content_tuesday), mGastroLocation.getOtTue()},
                 {getString(R.string.gastro_details_opening_hours_content_wednesday), mGastroLocation.getOtWed()},
@@ -74,22 +74,22 @@ public class GastroDetailsFragment extends Fragment {
                 {getString(R.string.gastro_details_opening_hours_content_saturday), mGastroLocation.getOtSat()},
                 {getString(R.string.gastro_details_opening_hours_content_sunday), mGastroLocation.getOtSun()}
         };
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 0; i < openingHoursData.length; i++) {
             // view day of the week
             TextView vDayOfTheWeek = new TextView(getActivity());
             vDayOfTheWeek.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, getPx(36)));
             vDayOfTheWeek.setEllipsize(TextUtils.TruncateAt.END);
             vDayOfTheWeek.setGravity(Gravity.BOTTOM);
             vDayOfTheWeek.setMaxLines(1);
-            vDayOfTheWeek.setText(data[i][0]);
+            vDayOfTheWeek.setText(openingHoursData[i][0]);
             vDayOfTheWeek.setTextColor(getResources().getColor(R.color.theme_primary));
             gastroDetailsOpeningHoursContent.addView(vDayOfTheWeek);
             // view opening hours
             TextView vOpeningHours = new TextView(getActivity());
             vOpeningHours.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             vOpeningHours.setMaxLines(1);
-            if (!data[i][1].trim().equals("")) {
-                vOpeningHours.setText(data[i][1]);
+            if (!openingHoursData[i][1].trim().equals("")) {
+                vOpeningHours.setText(openingHoursData[i][1]);
             } else {
                 vOpeningHours.setText(getString(R.string.gastro_details_opening_hours_content_closed));
             }
@@ -146,7 +146,7 @@ public class GastroDetailsFragment extends Fragment {
         } else if (i == 5) {
             return getString(R.string.gastro_details_miscellaneous_content_completely_vegan);
         }
-        return "";
+        return getString(R.string.gastro_details_miscellaneous_content_unknown);
     }
 
     private String getMiscellaneousContentString(int i) {
@@ -154,10 +154,8 @@ public class GastroDetailsFragment extends Fragment {
             return getString(R.string.gastro_details_miscellaneous_content_yes);
         } else if (i == 0) {
             return getString(R.string.gastro_details_miscellaneous_content_no);
-        } else if (i == -1) {
-            return getString(R.string.gastro_details_miscellaneous_content_unknown);
         }
-        return "";
+        return getString(R.string.gastro_details_miscellaneous_content_unknown);
     }
 
     private int getPx(int dimensionDp) {
