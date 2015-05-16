7d525765dae5df8ef7696d5441b80ee7f7649fa1
Julian Strobl <stroblju@gmail.com>
2015-05-16 20:03:56 +0200

    allow reporting errors in gastro activity
    A user can select an option from the menu in gastro activity. This fires
    Intent.ACTION_SENDTO and sends the user to his/her mail program, where
    he/she is presented a pre filled form.
    
    Also add the icon for reporting a problem from
    http://google.github.io/material-design-icons/
    
    Closes #11

--- app/src/main/java/org/berlin_vegan/bvapp/GastroActivity.java
+++ app/src/main/java/org/berlin_vegan/bvapp/GastroActivity.java
@@ -1,12 +1,19 @@
 package org.berlin_vegan.bvapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.samples.apps.iosched.ui.widget.SlidingTabLayout;
 
 public class GastroActivity extends BaseActivity {
+
+    private static final int NUM_STARS = 25;
 
     private GastroLocation mGastroLocation;
 
@@ -42,19 +49,76 @@ public class GastroActivity extends BaseActivity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
         Toolbar toolbar = getToolbar();
-
         if (toolbar != null) {
             toolbar.setTitle(mGastroLocation.getName());
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_gastro_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_report_error:
+                final Intent report = new Intent(Intent.ACTION_SENDTO);
+                final StringBuilder uriText = new StringBuilder()
+                        .append("mailto:")
+                        .append(Uri.encode("bvapp@berlin-vegan.org"))
+                        .append("?subject=")
+                        .append(Uri.encode(getMessageSubject()))
+                        .append("&body=")
+                        .append(Uri.encode(getMessageBody()));
+                final Uri uri = Uri.parse(uriText.toString());
+                report.setData(uri);
+                startActivity(Intent.createChooser(report, getString(R.string.action_report_error)));
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putSerializable("GASTRO_LOCATION", mGastroLocation);
         super.onSaveInstanceState(savedInstanceState);
+    }
+
+    private String getMessageSubject() {
+        StringBuilder subject = new StringBuilder()
+                .append(getString(R.string.error))
+                .append(": ")
+                .append(mGastroLocation.getName())
+                .append(", ")
+                .append(mGastroLocation.getStreet());
+        return subject.toString();
+    }
+
+    private String getMessageBody() {
+        String stars = "";
+        for (int i = 0; i < NUM_STARS; i++) {
+            stars += "*";
+        }
+        final StringBuilder body = new StringBuilder()
+                .append(stars)
+                .append("\nApp Version: ")
+                .append(BuildConfig.VERSION_GIT_DESCRIPTION)
+                .append("\nDevice Name: ")
+                .append(Build.MODEL)
+                .append("\nPlatform: Android")
+                .append("\nDevice Version: ")
+                .append(Build.VERSION.RELEASE)
+                .append("\n")
+                .append(stars)
+                .append("\n\n")
+                .append(getString(R.string.insert_error_report))
+                .append("\n\n");
+        return body.toString();
     }
 }
