423b60bc40176f585da11799f01571b378e3eda6
Julian Strobl <stroblju@gmail.com>
2015-06-01 18:33:31 +0200

    send crash reports only in release builds

--- app/src/main/java/org/berlin_vegan/bvapp/MainApplication.java
+++ app/src/main/java/org/berlin_vegan/bvapp/MainApplication.java
@@ -1,6 +1,7 @@
 package org.berlin_vegan.bvapp;
 
 import android.app.Application;
+import android.util.Log;
 
 import org.acra.ACRA;
 import org.acra.ReportingInteractionMode;
@@ -13,10 +14,15 @@ import org.acra.annotation.ReportsCrashes;
 
 public class MainApplication extends Application {
 
+    private static final String TAG = "MainApplication";
+
     @Override
     public void onCreate() {
         super.onCreate();
-        ACRA.init(this);
-        ACRA.getErrorReporter().setReportSender(new ACRAPostSender());
+        if (!BuildConfig.DEBUG) {
+            Log.i(TAG, "initialize application crash reporting");
+            ACRA.init(this);
+            ACRA.getErrorReporter().setReportSender(new ACRAPostSender());
+        }
     }
 }
