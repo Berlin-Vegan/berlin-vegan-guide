6b508f2b8cfa1e05fbdfc28c697409fd65dc10d3
Julian Strobl <stroblju@gmail.com>
2015-06-26 18:07:24 +0200

    add address to gastro details
    Reference #20

--- app/src/main/java/org/berlin_vegan/bvapp/fragments/GastroDetailsFragment.java
+++ app/src/main/java/org/berlin_vegan/bvapp/fragments/GastroDetailsFragment.java
@@ -3,9 +3,14 @@ package org.berlin_vegan.bvapp.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import org.berlin_vegan.bvapp.R;
 import org.berlin_vegan.bvapp.activities.GastroActivity;
@@ -33,7 +38,35 @@ public class GastroDetailsFragment extends Fragment {
         } else {
             mGastroLocation = (GastroLocation) savedInstanceState.getSerializable(GastroActivity.EXTRA_GASTRO_LOCATION);
         }
+
+        addAddress(v);
+
         return v;
+    }
+
+    private void addAddress(final View v) {
+        final RelativeLayout item = (RelativeLayout) v.findViewById(R.id.address);
+
+        final ImageView icon = (ImageView) item.findViewById(R.id.icon);
+        icon.setImageDrawable(getResources().getDrawable(R.mipmap.ic_place_white_24dp));
+        icon.setColorFilter(getResources().getColor(R.color.theme_primary));
+
+        final String text;
+        final String street = mGastroLocation.getStreet();
+        final int cityCode = mGastroLocation.getCityCode();
+        final String city = mGastroLocation.getCity();
+        if (street != null) {
+            text = "<a href=\"http://maps.google.com/maps?q="
+                    // google maps
+                    + street + ", " + cityCode + ", " + city + "\">"
+                    // view
+                    + street + ", " + cityCode + " " + city + "</a>";
+        } else {
+            text = getString(R.string.gastro_details_contact_address);
+        }
+        final TextView content = (TextView) item.findViewById(R.id.content);
+        content.setText(Html.fromHtml(text));
+        content.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     @Override
