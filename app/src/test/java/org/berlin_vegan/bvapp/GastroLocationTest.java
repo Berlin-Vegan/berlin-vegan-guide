60148be343119402c9b87740f576574b37abb397
Julian Strobl <stroblju@gmail.com>
2015-07-15 09:42:36 +0200

    test our productive json file
    By removing the JSON file under 'test/' we force gradle to use the one
    under 'main/'. This is the productive one, which is presented the user,
    if no internet connection is available.

--- app/src/test/java/org/berlin_vegan/bvapp/GastroLocationTest.java
+++ app/src/test/java/org/berlin_vegan/bvapp/GastroLocationTest.java
@@ -2,7 +2,6 @@ package org.berlin_vegan.bvapp;
 
 import org.berlin_vegan.bvapp.activities.MainListActivity;
 import org.berlin_vegan.bvapp.data.GastroLocation;
-import org.berlin_vegan.bvapp.data.GastroLocationPicture;
 import org.junit.Test;
 
 import java.io.InputStream;
@@ -18,14 +17,15 @@ public class GastroLocationTest {
     public void testParseLocationsWithGSON() throws Exception {
         final InputStream inputStream = getClass().getResourceAsStream(GASTRO_LOCATIONS_JSON);
         final List<GastroLocation> locationList = MainListActivity.createList(inputStream);
+        final int numLocations = locationList.size();
 
-        assertEquals(3, locationList.size());
-        assertEquals("arleo:", locationList.get(0).getName());
+        assertEquals(276, numLocations);
 
-        final GastroLocation location = locationList.get(1);// attis
-        final List<GastroLocationPicture> pictures = location.getPictures();
-        assertEquals(2, pictures.size()); // 2 pictures
-        System.out.println(pictures.get(0).getUrl());
-        System.out.println(pictures.get(0).getWidth());
+        final GastroLocation location = locationList.get(0);
+        final String name = location.getName();
+        final int numPictures = location.getPictures().size();
+
+        assertEquals("Amaranth", name);
+        assertEquals(2, numPictures);
     }
 }
