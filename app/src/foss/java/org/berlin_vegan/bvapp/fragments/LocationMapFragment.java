3e6abc8c97f6911638cc0918fefc147bbb2c3756
micu <micuintus@gmx.de>
2015-12-10 01:29:10 +0100

    Switch default zoom to 15 in the OSMdroid version
    for getting a better overview at the beginning
    (when the detail view of a location is opened).

--- app/src/foss/java/org/berlin_vegan/bvapp/fragments/LocationMapFragment.java
+++ app/src/foss/java/org/berlin_vegan/bvapp/fragments/LocationMapFragment.java
@@ -61,7 +61,7 @@ public class LocationMapFragment extends Fragment {
     public void setLocation(Location location) {
 
         IMapController mapController = mMapView.getController();
-        mapController.setZoom(20);
+        mapController.setZoom(15);
         GeoPoint gPoint = new GeoPoint(location.getLatCoord(), location.getLongCoord());
         mapController.setCenter(gPoint);
 
